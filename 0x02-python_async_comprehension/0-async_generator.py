0108d1362228fa1077fc05c04b36d667838716a5
Ifiok Ekott <ekottifiok@gmail.com>
2023-04-05 01:46:13 +0100

    initial commit for 0x02

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Task 0: Async Generator
+"""
+from asyncio import sleep
+from random import random
+from typing import AsyncIterator
+
+
+async def async_generator() -> AsyncIterator[float]:
+    """Write a coroutine called async_generator that takes no arguments."""
+    for _ in range(10):
+        await sleep(1)
+        yield random() * 10
---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Run time for four parallel comprehensions"""
+from asyncio import gather
+from time import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+async def measure_runtime() -> float:
+    """
+    Import async_comprehension from the previous file and write 
+    a measure_runtime coroutine that will execute async_comprehension
+    four times in parallel using asyncio.gather.
+
+    measure_runtime should measure the total runtime and return it.
+    """
+    start_time = time()
+    gather(*(async_comprehension() for _ in range(4)))
+    return time() - start_time
