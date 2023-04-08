3d3564769975427a86f184b3122e60a95c7d833b
Ifiok Ekott <ekottifiok@gmail.com>
2023-04-08 05:36:41 +0100

    added a readme

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
-"""A basic async syntax"""
+"""The basics of async"""
 from asyncio import sleep
 from random import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """waits for a random number of seconds"""
+    """
+    Write an asynchronous coroutine that takes in an integer argument
+    (max_delay, with a default value of 10) named wait_random that waits
+    for a random delay between 0 and max_delay (included and float value)
+    seconds and eventually returns it.
+
+    Use the random module.
+    """
     wait_seconds: float = random() * max_delay
     await sleep(wait_seconds)
     return wait_seconds
